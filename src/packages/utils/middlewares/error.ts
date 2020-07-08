b810cc19645324bc1f1a92df16202e15c07c0619
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-07-09 08:42:55 +0900

    Fix errorHandler bug

--- src/packages/utils/middlewares/error.ts
+++ src/packages/utils/middlewares/error.ts
@@ -3,9 +3,15 @@ import { ServerError } from "@packages/error";
 import { InternalServerError } from "@packages/error/Internal";
 
 const errorMiddleware: ErrorRequestHandler = (err, req, res, next) => {
-  const error = err instanceof ServerError ? err : InternalServerError;
+  const error = err.status && err.code ? err : InternalServerError;
 
-  res.status(error.getStatus()).json(error.getResponse());
+  console.log(error);
+
+  res.status(error.status).json({
+    message: error.message,
+    code: error.code,
+    status: error.status,
+  });
 };
 
 export default errorMiddleware;
