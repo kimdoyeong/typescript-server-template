f1c1c806de876575752f03482b34f51714eb254a
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-07-09 09:27:05 +0900

    Remove unnecessary imports and logging

--- src/packages/utils/middlewares/error.ts
+++ src/packages/utils/middlewares/error.ts
@@ -1,12 +1,8 @@
 import { ErrorRequestHandler } from "express";
-import { ServerError } from "@packages/error";
 import { InternalServerError } from "@packages/error/Internal";
 
 const errorMiddleware: ErrorRequestHandler = (err, req, res, next) => {
   const error = err.status && err.code ? err : InternalServerError;
-
-  console.log(error);
-
   res.status(error.status).json({
     message: error.message,
     code: error.code,
