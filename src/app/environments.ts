aa26422d690f4509a954b8f1ed0b05ef20652048
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-07-09 12:53:34 +0900

    Add enviroments

--- src/app/environments.ts
+++ src/app/environments.ts
@@ -1,5 +1,7 @@
 const environments = {
   PORT: (process.env.PORT && parseInt(process.env.PORT, 10)) || 4000,
+  MONGO_URL: process.env.MONGO_URL || "mongodb://mongo/ts-template",
+  NODE_ENV: process.env.NODE_ENV || "development",
 };
 
 export default environments;
