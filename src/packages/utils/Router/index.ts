0f610731db26e6d10cf53f5d558a19f7256ff07d
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-07-10 09:35:06 +0900

    Update Router

--- src/packages/utils/Router/index.ts
+++ src/packages/utils/Router/index.ts
@@ -5,34 +5,30 @@ class Router {
   public router = express.Router();
 
   public static routes<T extends RequestHandler>(
-    target: Router,
+    target: any,
     key: string,
     descriptor: TypedPropertyDescriptor<T>
   ) {
     const requestHandler = descriptor.value;
     if (!requestHandler) return;
 
-    (descriptor.value as any) = (
-      req: Request,
-      res: Response,
-      next: NextFunction
-    ) => {
-      requestHandler(req, res, next)
-        .then((data: RouterReturnType | any) => {
-          data.status && res.status(data.status);
-          res.json(data.data || { success: true });
-        })
-        .catch((e) => {
-          next(e);
-        });
+    (descriptor.value as any) = this.createRequestHandler(requestHandler);
+  }
+  public static createRequestHandler(
+    handler: (req: Request, res: Response, next: NextFunction) => any
+  ) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const data = await Promise.resolve(handler(req, res, next));
+        data.status && res.status(data.status);
+        res.json(data.data || { success: true });
+      } catch (e) {
+        next(e);
+      }
     };
   }
 }
 
-type RequestHandler = (
-  req: Request,
-  res: Response,
-  next?: NextFunction
-) => Promise<RouterReturnType | any>;
+type RequestHandler = (req: Request, res: Response, next?: NextFunction) => any;
 
 export default Router;
