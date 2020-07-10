03911380ee4cc48570a13fe666657f649f90e152
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-07-10 09:35:29 +0900

    Update error middleware

--- src/packages/utils/middlewares/error.ts
+++ src/packages/utils/middlewares/error.ts
@@ -1,8 +1,11 @@
 import { ErrorRequestHandler } from "express";
 import { InternalServerError } from "@packages/error/Internal";
+import Logger from "../logger";
 
 const errorMiddleware: ErrorRequestHandler = (err, req, res, next) => {
   const error = err.status && err.code ? err : InternalServerError;
+
+  if (!err.status && !err.code) Logger.error(err);
   res.status(error.status).json({
     message: error.message,
     code: error.code,
